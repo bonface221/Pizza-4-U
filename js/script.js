486ae40e68e3bc30170cb9381d80bb6729b8d293
[bonface221] <[mainaboniface221@gmail.com]>
2022-03-20 20:43:07 +0300

    deleted an Image I was not needing anymore

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let price = 500;
+
+let crust_price, topping_price;
+function Obtainpizza(name, size, crust, topping, total) {
+    this.name = name;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+    this.total = total;
+
+}
+
+$(document).ready(function () {
+    $("#proceed").click
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
