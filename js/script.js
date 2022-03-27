1264773acc5822b8d524081d0218c9a45b7c7b66
[bonface221] <[mainaboniface221@gmail.com]>
2022-03-27 22:14:10 +0300

    styling:optimized styling of the small screens

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 // let price = 500;
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+	menu.classList.toggle("fa-times");
+	navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+	menu.classList.remove("fa-times");
+	navbar.classList.remove("active");
+};
+
+
+
 
 let price, crust_price, topping_price;
 let total = 0;
